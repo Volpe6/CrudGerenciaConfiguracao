231d4c25a19ce8d7b005ea294ab457af7be6aa56
Volpe6 <baasch2014@gmail.com>
2020-09-06 17:19:25 -0300

    cria backend da versao2 #2

---
+++ src/model/cliente.js
@@ -0,0 +1,47 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Cliente extends Model {}
+
+Cliente.init({
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cpf: {
+        type: DataTypes.STRING(11),
+        allowNull: false
+    },
+    logradouro: {
+        type: DataTypes.STRING,
+    },
+    numero: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    bairro: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cidade: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cep: {
+        type: DataTypes.STRING(8),
+        allowNull: false
+    },
+    estado: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, {
+    //outras opções do modelo
+    sequelize,//instancia da conexao
+    modelName: 'Cliente',
+    tableName: 'tbcliente',
+    timestamps: false
+});
+
+
+module.exports = Cliente;
